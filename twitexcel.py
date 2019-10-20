2158ae84622abaee09999c6b7656b8e6426b19b5
teowyong <teowyong89@gmail.com>
2019-10-20 15:09:03 +0800

    template for seaborn charts in excel

--- twitexcel.py
+++ twitexcel.py
@@ -1,6 +1,10 @@
 import xlwings as xw
 import os
 import tweepy
+import pandas as pd
+import seaborn as sns
+sns.set(style="ticks", color_codes=True)
+import numpy as np
 from datetime import timedelta
 from dotenv import load_dotenv
 
@@ -50,7 +54,31 @@ def main():
             sht.range('E'+str(ROW)).value = tweet.full_text
 
         ROW=ROW+1
-
+    
+    # Dataframe for charts
+    df = sht.range('A4').expand().options(pd.DataFrame).value
+    # Sentiment score placeholder
+    df['Sentiment Score'] = np.random.randint(0,100,size=(len(df),1))
+    # Sentiment score placeholder
+    index = ['Created At','Sentiment Score','Location']
+    df = df[index]
+    df_week = df
+    df_week['week'] = df_week['Created At'].dt.week
+    
+    #Box plot
+    box = sns.catplot(x='week', y='Sentiment Score', hue='Location', kind="box", data=df_week)
+    box.fig.set_size_inches(7, 3)
+    rng = sht.range("F1")
+    sht.pictures.add(box.fig, top=rng.top, left=rng.left, name='Box Plot', update = True)
+    
+    #Violin plot
+    box = sns.catplot(x='Location', y='Sentiment Score', kind="violin", data=df)
+    box.fig.set_size_inches(7, 3)
+    rng = sht.range("F16")
+    sht.pictures.add(box.fig, top=rng.top, left=rng.left, name='Violin Plot', update = True)
+    
 if __name__ == "__main__":
     xw.books.active.set_mock_caller()
     main()
+
+    
