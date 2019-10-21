d65639eb4996fdff50bd6a35e483f1748993ea2a
hys89 <10128883+hys89@users.noreply.github.com>
2019-10-21 23:51:50 +0800

    Updated UI
    Enabled searching by poster, hashtag(s) or search term(s) [at least one must be filled].
    Results placed in separate sheet.
    Added prompts for exceptions and completion of script.

--- twitexcel.py
+++ twitexcel.py
@@ -7,10 +7,33 @@ sns.set(style="ticks", color_codes=True)
 import numpy as np
 from datetime import timedelta
 from dotenv import load_dotenv
+import win32api
+import win32con
+
+def construct_query(poster, search_terms, hashtags):
+    
+    search_query = search_terms
+
+    if poster != None and search_query != None:
+    	search_query = str(search_query) + ' from:' + poster
+        
+    if poster != None and search_query == None:
+    	search_query = 'from:' + poster
+        
+    if hashtags != None and search_query != None:
+        hashtag_string = ['#'+h for h in hashtags.split()]
+        search_query = str(search_query) + ' ' + ' '.join(hashtag_string)
+        
+    if hashtags != None and search_query == None:
+        hashtag_string = ['#'+h for h in hashtags.split()]
+        search_query = ' '.join(hashtag_string)
+        
+    return search_query
 
 def main():
     wb = xw.Book.caller()
-    sht = wb.sheets("Sheet1")
+    in_sht = wb.sheets("User Interface")
+    out_sht = wb.sheets("Results")
     
     # Change working directory to read .env file properly
     path = wb.fullname
@@ -28,35 +51,65 @@ def main():
     access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
     
     # User Inputs
-    user_term = sht.range('B1').value
-    user_n = sht.range('B2').value
+    search_terms = in_sht.range('B10').value
+    hashtags = in_sht.range('B11').value
+    poster = in_sht.range('B12').value
+    user_n = in_sht.range('B13').value
+    
+    # Check for exceptions
+    if search_terms == None and poster == None and hashtags == None:
+    	win32api.MessageBox(xw.apps.active.api.Hwnd, 'Please key in at least one of the following: Search Term, Hashtag, Poster', 'Exception', win32con.MB_ICONINFORMATION)
+    	return
 
+    if user_n == None:
+    	win32api.MessageBox(xw.apps.active.api.Hwnd, 'Please key in the Maximum # of Tweets to be returned', 'Exception', win32con.MB_ICONINFORMATION)
+    	return
+    
+    # Construct Query
+    search_query = construct_query(poster, search_terms, hashtags)
+    
     # Scrape
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    results = tweepy.Cursor(api.search, q=user_term, tweet_mode='extended', lang='en').items(user_n)
+    results = tweepy.Cursor(api.search, q=search_query, tweet_mode='extended', lang='en').items(user_n)
+
+    # Prompt user if no results are found
+    if not(any(True for _ in results)):
+        win32api.MessageBox(xw.apps.active.api.Hwnd, 'No tweets found. Please refine the search criteria.', 'Exception', win32con.MB_ICONINFORMATION)
+        return
     
-    ROW=5
+    ROW=2
     
     # Clear Contents
-    sht.range(sht.range('A'+str(ROW)), sht.range('F'+str(ROW)).end('down')).clear_contents()
+    out_sht.range(out_sht.range('A'+str(ROW)), out_sht.range('F'+str(ROW)).end('down')).clear_contents()
         
     for tweet in results:
-        sht.range('A'+str(ROW)).value = tweet.id_str
-        sht.range('B'+str(ROW)).value = tweet.author._json['screen_name']
-        sht.range('C'+str(ROW)).value = tweet.author._json['location']
-        sht.range('D'+str(ROW)).value = tweet.created_at + timedelta(hours=8) # Set to local time
+        out_sht.range('A'+str(ROW)).value = tweet.id_str
+        out_sht.range('B'+str(ROW)).value = tweet.author._json['screen_name']
+        out_sht.range('C'+str(ROW)).value = tweet.author._json['location']
+        out_sht.range('D'+str(ROW)).value = tweet.created_at + timedelta(hours=8) # Set to local time
         
         if 'retweeted_status' in tweet._json:
-            sht.range('E'+str(ROW)).value = tweet._json['retweeted_status']['full_text']
+            out_sht.range('E'+str(ROW)).value = tweet._json['retweeted_status']['full_text']
         else:
-            sht.range('E'+str(ROW)).value = tweet.full_text
+            out_sht.range('E'+str(ROW)).value = tweet.full_text
 
         ROW=ROW+1
+
+
+    #######################################
+    ### Sentiment Scoring #################
+    #######################################
     
+
+
+    #######################################
+    ### Charts ############################
+    #######################################
+
     # Dataframe for charts
-    df = sht.range('A4').expand().options(pd.DataFrame).value
+    df = out_sht.range('A1').expand().options(pd.DataFrame).value
     # Sentiment score placeholder
     df['Sentiment Score'] = np.random.randint(0,100,size=(len(df),1))
     # Sentiment score placeholder
@@ -68,14 +121,22 @@ def main():
     #Box plot
     box = sns.catplot(x='week', y='Sentiment Score', hue='Location', kind="box", data=df_week)
     box.fig.set_size_inches(7, 3)
-    rng = sht.range("F1")
-    sht.pictures.add(box.fig, top=rng.top, left=rng.left, name='Box Plot', update = True)
+    rng = out_sht.range("F1")
+    out_sht.pictures.add(box.fig, top=rng.top, left=rng.left, name='Box Plot', update = True)
     
     #Violin plot
     box = sns.catplot(x='Location', y='Sentiment Score', kind="violin", data=df)
     box.fig.set_size_inches(7, 3)
-    rng = sht.range("F16")
-    sht.pictures.add(box.fig, top=rng.top, left=rng.left, name='Violin Plot', update = True)
+    rng = out_sht.range("F16")
+    out_sht.pictures.add(box.fig, top=rng.top, left=rng.left, name='Violin Plot', update = True)
+
+
+    #######################################
+    ### Prompt Completion #################
+    #######################################
+
+    win32api.MessageBox(xw.apps.active.api.Hwnd, 'The sentiment analysis is complete! Check out the Results sheet!', 'Done', win32con.MB_ICONINFORMATION)
+
     
 if __name__ == "__main__":
     xw.books.active.set_mock_caller()
