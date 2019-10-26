1491241bc8c0c933efcbd316bd57827602ea78a7
hys89 <10128883+hys89@users.noreply.github.com>
2019-10-26 20:39:40 +0800

    Updated twitexcel.py
    Added option to exclude retweets.
    Color to be based on excel conditional format instead.
    Two buttons to direct user to Tweets or Dashboard respectively.
    Use of pandas df to store tweets.

--- twitexcel.py
+++ twitexcel.py
@@ -9,12 +9,13 @@ from dotenv import load_dotenv
 import win32api
 import win32con
 import spacy
+import re
 from NLTKVader import vader_compound_score
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 sns.set(style="ticks", color_codes=True)
 
-def construct_query(poster, search_terms, hashtags):
+def construct_query(poster, search_terms, hashtags, retweet):
     
     search_query = search_terms
 
@@ -31,39 +32,17 @@ def construct_query(poster, search_terms, hashtags):
     if hashtags != None and search_query == None:
         hashtag_string = ['#'+h for h in hashtags.split()]
         search_query = ' '.join(hashtag_string)
-        
-    return search_query
-
-def cell_color(sentiment):
-    if -1 <= sentiment < -0.8:
-        color = (165,0,38)
-    elif -0.8 <= sentiment < -0.6:
-        color = (215,48,39)
-    elif -0.6 <= sentiment < -0.4:
-        color = (244,109,67)
-    elif -0.4 <= sentiment < -0.2:
-        color = (253,174,97)
-    elif -0.2 <= sentiment < 0:
-        color = (254,224,139)
-    elif sentiment == 0:
-        color = (255,255,191)
-    elif 0 < sentiment <= 0.2:
-        color = (217,239,139)
-    elif 0.2 < sentiment <= 0.4:
-        color = (166,217,106)
-    elif 0.4 < sentiment <= 0.6:
-        color = (102,189,99)
-    elif 0.6 < sentiment <= 0.8:
-        color = (26,152,80)
-    elif 0.8 < sentiment <= 1:
-        color = (0,104,55)
-    return color
     
+    if retweet == "No":
+        search_query = search_query + " -filter:retweets"
+        
+    return search_query    
 
-def main():
+def main(dest):
     wb = xw.Book.caller()
     in_sht = wb.sheets("User Interface")
-    out_sht = wb.sheets("Results")
+    out_sht = wb.sheets("Tweets")
+    viz_sht = wb.sheets("Dashboard")
     
     # Change working directory to read .env file properly
     path = wb.fullname
@@ -85,6 +64,7 @@ def main():
     hashtags = in_sht.range('B11').value
     poster = in_sht.range('B12').value
     user_n = in_sht.range('B13').value
+    retweet = in_sht.range('B14').value
     
     # Check for exceptions
     if search_terms == None and poster == None and hashtags == None:
@@ -96,46 +76,66 @@ def main():
         return
 
     if user_n == None:
-    	win32api.MessageBox(xw.apps.active.api.Hwnd, 'Please key in the Maximum # of Tweets to be returned', 'Exception', win32con.MB_ICONINFORMATION)
+    	win32api.MessageBox(xw.apps.active.api.Hwnd, 'Please key in the Maximum # of Tweets to be returned.', 'Exception', win32con.MB_ICONINFORMATION)
     	return
     
+    if retweet == None:
+        win32api.MessageBox(xw.apps.active.api.Hwnd, 'Please indicate whether retweets should be included.', 'Exception', win32con.MB_ICONINFORMATION)
+
     # Construct Query
-    search_query = construct_query(poster, search_terms, hashtags)
+    search_query = construct_query(poster, search_terms, hashtags, retweet)
     
     # Scrape
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     results = tweepy.Cursor(api.search, q=search_query, tweet_mode='extended', lang='en').items(user_n+1)
-
+        
     # Prompt user if no results are found
     if not(any(True for _ in results)):
         win32api.MessageBox(xw.apps.active.api.Hwnd, 'No tweets found. Please refine the search criteria.', 'Exception', win32con.MB_ICONINFORMATION)
         return
     
-    ROW=2
-    
     # Clear Contents
-    out_sht.range(out_sht.range('A'+str(ROW)), out_sht.range('F'+str(ROW)).end('down')).clear_contents()
-    out_sht.range(out_sht.range('F'+str(ROW)), out_sht.range('F'+str(ROW)).end('down')).color = None
-        
+    out_sht.range(out_sht.range('A2'), out_sht.range('H2').end('down')).clear_contents()
+
+    results_list = []
     for tweet in results:
-        out_sht.range('A'+str(ROW)).value = tweet.id_str
-        out_sht.range('B'+str(ROW)).value = tweet.author._json['screen_name']
-        out_sht.range('C'+str(ROW)).value = tweet.author._json['location']
-        out_sht.range('D'+str(ROW)).value = tweet.created_at + timedelta(hours=8) # Set to local time
+        tweet_info = {}
+        tweet_info['ID'] = tweet.id_str
+        tweet_info['Created At'] = tweet.created_at + timedelta(hours=8) # Set to local time
+        tweet_info['Screen Name'] = tweet.author._json['screen_name']
+        tweet_info['User Location'] = tweet.author._json['location']
+        tweet_info['Followers'] = tweet.author._json['followers_count']
+        tweet_info['Following'] = tweet.author._json['friends_count']
         
         if 'retweeted_status' in tweet._json:
             tweet_text = tweet._json['retweeted_status']['full_text']
         else:
             tweet_text = tweet.full_text
-        out_sht.range('E'+str(ROW)).value = tweet_text
         
-        vader_score = vader_compound_score(tweet_text)
-        out_sht.range('F'+str(ROW)).value = vader_score
-        out_sht.range('F'+str(ROW)).color = cell_color(vader_score)
-
-        ROW=ROW+1
+        tweet_info['Full Text'] = re.sub(r"http\S+", "", tweet_text) # Clean http links
+        tweet_info['VADER Sentiment'] = vader_compound_score(tweet_text)
+        results_list.append(tweet_info)
+    
+    results_df = pd.DataFrame(results_list, columns=tweet_info.keys())
+    out_sht.range('A1').options(index=False).value = results_df
+    
+    # Column Widths
+    out_sht.range('A1').column_width = 11.25 #ID
+    out_sht.range('B1').column_width = 14.88 #Created At
+    out_sht.range('C1').column_width = 16.25 #Screen Name
+    out_sht.range('D1').column_width = 25    #User Location
+    out_sht.range('E1').column_width = 10.25 #Followers
+    out_sht.range('F1').column_width = 10.25 #Following
+    out_sht.range('G1').column_width = 83    #Full Text
+    out_sht.range('H1').column_width = 17.5  #VADER
+    
+    # Autofit Rows
+    out_sht.autofit("rows")
+    
+    # Bold Headers
+    out_sht.range('1:1').api.Font.Bold = True
 
     #######################################
     ### Charts ############################
@@ -150,7 +150,7 @@ def main():
     
     # Dataframe for charts
     df = out_sht.range('A1').expand().options(pd.DataFrame).value
-    index = ['Created At','VADER Sentiment','Location', 'Full Text']
+    index = ['Created At','VADER Sentiment','User Location', 'Full Text']
     df = df[index]
     df['week'] = df['Created At'].dt.week
     df['score_category'] = score_groups(df['VADER Sentiment'])
@@ -162,8 +162,8 @@ def main():
     wordcloud = WordCloud(stopwords=stopwords, background_color="white").generate(text)
     plt.axis("off")
     wordcloud_fig = plt.imshow(wordcloud, interpolation='bilinear').get_figure()
-    rng = out_sht.range("G1")
-    out_sht.pictures.add(wordcloud_fig, top=rng.top, left=rng.left, name='Word Cloud', update = True)
+    rng = viz_sht.range("a1")
+    viz_sht.pictures.add(wordcloud_fig, top=rng.top, left=rng.left, name='Word Cloud', update = True)
     
     #Bar plot
     df_score = df.groupby(['score_category']).size().reset_index().rename(columns={0:'counts'})
@@ -171,8 +171,8 @@ def main():
     bar = sns.catplot(x="score_category", y="counts", order = order, hue="score_category", kind="bar", palette = "RdBu", data=df_score)
     bar.set_xticklabels(rotation=30)
     bar.fig.set_size_inches(7, 3)
-    rng = out_sht.range("G11")
-    out_sht.pictures.add(bar.fig, top=rng.top, left=rng.left, name='Bar Plot', update = True)
+    rng = viz_sht.range("A33")
+    viz_sht.pictures.add(bar.fig, top=rng.top, left=rng.left, name='Bar Plot', update = True)
     
     #Box plot
 #    box = sns.catplot(x='week', y='VADER Sentiment', hue='Location', kind="box", data=df)
@@ -190,12 +190,22 @@ def main():
     #######################################
     ### Prompt Completion #################
     #######################################
-
-    win32api.MessageBox(xw.apps.active.api.Hwnd, 'The sentiment analysis is complete! Check out the Results sheet!', 'Done', win32con.MB_ICONINFORMATION)
+    
+    if dest == "tweets":
+        out_sht.activate()
+    elif dest == "dashboard":
+        viz_sht.activate()
+        
+    win32api.MessageBox(xw.apps.active.api.Hwnd, 'The sentiment analysis is complete!', 'Done', win32con.MB_ICONINFORMATION)
 
     
 if __name__ == "__main__":
     xw.books.active.set_mock_caller()
     main()
+    
+    
+    
+    
+    
 
     
